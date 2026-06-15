0b090a95c53e55bad285112098cb4fcdcc15ea89
Your Name <chiragbhora1@outlook.com>
2026-06-15 08:35:39 +0530

    Added conditional resource configuration

--- js/form.js
+++ js/form.js
@@ -91,43 +91,115 @@ deploymentType.addEventListener("change", () => {
 
     else if(value === "infrastructure"){
 
-        dynamicFields.innerHTML = `
-            <label>IaC Tool</label>
-            <select id="iac">
-                <option>Terraform</option>
-            </select>
+    dynamicFields.innerHTML = `
+        <label>IaC Tool</label>
+        <select id="iac">
+            <option>Terraform</option>
+        </select>
 
-            <label>Cloud Provider</label>
-            <select id="cloud">
-                <option>Azure</option>
-            </select>
+        <label>Cloud Provider</label>
+        <select id="cloud">
+            <option>Azure</option>
+        </select>
 
-            <label>Resources</label>
+        <label>Resources</label>
 
-            <div class="checkbox-group">
+        <div class="checkbox-group">
 
-                <label>
-                    <input type="checkbox">
-                    Resource Group
-                </label>
+            <label>
+                <input type="checkbox" id="rgCheck">
+                Resource Group
+            </label>
 
-                <label>
-                    <input type="checkbox">
-                    Virtual Network
-                </label>
+            <label>
+                <input type="checkbox" id="vmCheck">
+                Virtual Machine
+            </label>
 
-                <label>
-                    <input type="checkbox">
-                    Virtual Machine
-                </label>
+            <label>
+                <input type="checkbox" id="storageCheck">
+                Storage Account
+            </label>
 
-                <label>
-                    <input type="checkbox">
-                    Storage Account
-                </label>
+        </div>
 
-            </div>
+        <div id="resourceConfig"></div>
         `;
     }
+
+});
+document.addEventListener("change", (event) => {
+
+    const configDiv =
+    document.getElementById("resourceConfig");
+
+    if(!configDiv) return;
+
+    let html = "";
+
+    const rgCheck =
+    document.getElementById("rgCheck");
+
+    const vmCheck =
+    document.getElementById("vmCheck");
+
+    const storageCheck =
+    document.getElementById("storageCheck");
+
+    if(rgCheck && rgCheck.checked){
+
+        html += `
+            <h3>Resource Group</h3>
+
+            <label>Resource Group Name</label>
+            <input type="text" id="rgName">
+
+            <label>Location</label>
+            <select id="location">
+                <option>Central India</option>
+                <option>East US</option>
+                <option>West Europe</option>
+            </select>
+        `;
+    }
+
+    if(vmCheck && vmCheck.checked){
+
+        html += `
+            <h3>Virtual Machine</h3>
+
+            <label>VM Name</label>
+            <input type="text" id="vmName">
+
+            <label>VM Size</label>
+            <select id="vmSize">
+                <option>Standard_B1s</option>
+                <option>Standard_B2s</option>
+                <option>Standard_D2s_v3</option>
+            </select>
+
+            <label>Admin Username</label>
+            <input type="text" id="adminUser">
+        `;
+    }
+
+    if(storageCheck && storageCheck.checked){
+
+        html += `
+            <h3>Storage Account</h3>
+
+            <label>Storage Name</label>
+            <input type="text" id="storageName">
+
+            <label>Replication</label>
+            <select id="replication">
+                <option>LRS</option>
+                <option>GRS</option>
+                <option>ZRS</option>
+            </select>
+        `;
+    }
+
+    configDiv.innerHTML = html;
 
 });
