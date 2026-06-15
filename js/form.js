fba5068acf9ea35917e84869ce94376fa457dc05
Your Name <chiragbhora1@outlook.com>
2026-06-15 11:50:42 +0530

    Add for download button

--- js/form.js
+++ js/form.js
@@ -314,3 +314,28 @@ document
 
     link.click();
 });
+document
+.getElementById("copyBtn")
+.addEventListener("click", async () => {
+
+    const content =
+        document.getElementById("terraformOutput").value;
+
+    if (!content) {
+        alert("Generate output first");
+        return;
+    }
+
+    try {
+
+        await navigator.clipboard.writeText(content);
+
+        alert("Output copied successfully!");
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Copy failed");
+    }
+});
