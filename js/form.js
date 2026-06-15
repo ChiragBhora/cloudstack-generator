cb0996f714a8015dfce48b4e477618daad837e4f
Your Name <chiragbhora1@outlook.com>
2026-06-15 11:34:44 +0530

    Add for download button

--- js/form.js
+++ js/form.js
@@ -278,3 +278,39 @@ steps:
   displayName: Deploy to VM
 `;
 }
+document
+.getElementById("downloadBtn")
+.addEventListener("click", () => {
+
+    const content =
+        document.getElementById("terraformOutput").value;
+
+    if (!content) {
+        alert("Generate output first");
+        return;
+    }
+
+    const deployment =
+        document.getElementById("deploymentType").value;
+
+    const filename =
+        deployment === "application"
+            ? "azure-pipelines.yml"
+            : "main.tf";
+
+    const blob = new Blob(
+        [content],
+        { type: "text/plain" }
+    );
+
+    const link =
+        document.createElement("a");
+
+    link.href =
+        URL.createObjectURL(blob);
+
+    link.download =
+        filename;
+
+    link.click();
+});
